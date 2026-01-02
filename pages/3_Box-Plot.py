cfdf9799f5afedcc3003685746329c6598eb2f52
Mohamed Elhasany <elhasanymohamed123@gmail.com>
2026-01-02 03:00:16 -0500

    fix_box_plot issue

--- pages/3_Box-Plot.py
+++ pages/3_Box-Plot.py
@@ -1,10 +1,11 @@
 # pages/3_Box-Plot.py
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from src.cleaning import load_data 
 from src.sidebar import show_global_slicers
 from src.plotting import generate_boxplot
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 # ---------- guarantee data ----------
 if "df" not in st.session_state:
